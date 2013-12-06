940a3265a8570dcf4879d398b25bf0efaa738c69
Jake Madison <jakemadison@gmail.com>
2013-12-06 15:52:52 -0800

    ugh, i think there might be too many variables unaccounted for to actually test this properly.

--- eph1.py
+++ eph1.py
@@ -1,19 +1,15 @@
 # this script is an attempt to test a client report of "no sockets" to confirm if they are not
-# not receiving data, because they have run out of socket space on their side
+# not receiving data because they have run out of socket space on their side
 # because of TCP TIME_WAITs.
 # see: http://www.ncftp.com/ncftpd/doc/misc/ephemeral_ports.html
-
-
 
 #create a connection class for every line item.
 #that class has a TTL which counts down until zero and then dies
 #we have max connections = 4000 as a global
 #and we have total time which just counts up from start time.
 
-
-#okay, this is better done either as threads (gross) or with the main event loop in the outer block
-#and a simple method to decrement
-import time
+#okay so problems here: change in network speed, number of other requests going on
+#anything DB related, anything else client-related
 
 max_conn = 4000
 mainclock = 1500
@@ -22,11 +18,10 @@ req_array = []
 
 class Request():
 
-    global_ttl = float(240) + float(5)
+    global_ttl = float(240) + float(5)  # 240 is IP stack standard TIME_WAIT
 
     def __init__(self, size):
-        self.ttl = float((Request.global_ttl) + (0.025*float(size)))
-        #print self.ttl
+        self.ttl = float(Request.global_ttl + (0.025*float(size)))  # size needs tweaking.
         self.active = True
 
     def decrement_life(self):
@@ -38,12 +33,16 @@ class Request():
         print self.ttl
 
 
+# add a new request to our array, decrement max connections
 def make_req(size):
     req_array.append(Request(size))
     global max_conn
     max_conn -= 1
 
 
+#move forward in time, decrease everyone's life
+#then for any that are dead now, set active to false and
+#get rid of them.
 def advance_time():
 
     global max_conn
@@ -58,6 +57,7 @@ def advance_time():
                 max_conn += 1
 
 
+#convert our timestamps to seconds:
 def stamp_to_secs(stamp):
     if stamp:
         h, m, s = stamp.split(':')
@@ -65,24 +65,22 @@ def stamp_to_secs(stamp):
         return int(secs)
 
 
-
+#open our file, split up timestamp and size information
 f = open('./timesnew_sorted', 'r')
-
-
 
 thisone = f.readline().replace('\n', '')
 thistime, thissize = thisone.split(' ')
-
 nextval = stamp_to_secs(thistime)
 conold = max_conn
 
+
+#I don't care about the earlier values, skip to mass influx:
 starter = '16:01:48'
 
 #cue us up:
 while thistime.strip() != starter:
     thisone = f.readline().replace('\n', '')
     thistime, thissize = thisone.split(' ')
-
     nextval = stamp_to_secs(thistime)
 
 
@@ -90,67 +88,40 @@ while thistime.strip() != starter:
 lowestval = max_conn
 
 while True:
-
     while mainclock == nextval:
-        #print 'creating new instance'
+
         make_req(thissize)
+
+        # get a new line item:
         thisone = f.readline().replace('\n', '')
 
-        if '#' in thistime.strip():
+        #check for EOF marker
+        if '#' in thisone:
             print 'Made it through without breakage'
             print 'lowests', lowestval, lowesttime
             exit()
-
 
         thistime, thissize = thisone.split(' ')
         nextval = stamp_to_secs(thistime)
 
     advance_time()
 
+    #check for change in max conns and notify:
     if conold != max_conn:
         print 'cons: ', max_conn, 'clock: ', mainclock, 'time: ', thistime, 'size: ', thissize
         if max_conn < lowestval:
             lowestval = max_conn
             lowesttime = thistime
 
+    #see if we have run out of connections. (really this should be higher as well, but whatever)
     if max_conn <= 0:
         print 'BREAK BREAK BREAK!', max_conn
-        print 'lowests', lowestval, lowesttime
-
-
-        #for each in req_array:
-         #   print each.ttl
-
-
+        print 'lowest vals: ', lowestval, lowesttime
         exit()
 
 
     conold = max_conn
     mainclock += 1
-    #print mainclock
-
-
 
 
 f.close()
-
-
-
-
-
-#
-#
-# for x in range(0,10):
-#     print "creating instance     ", x
-#     req_array.append(Request())
-#     if len(req_array) > 0:
-#         for each in req_array:
-#             each.decrement_life()
-#
-#
-#
-#
-#
-# for each in req_array:
-#     print each.active
-#     print each.ttl
